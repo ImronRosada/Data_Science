bcf8812062caa7a0cd4285e9512e0866f6a5a0eb
ImronRosada <rosadaimron96@gmail.com>
2025-03-16 06:45:39 +0700

    Data Science Portfolio Project

--- Project_Portofolio/main.py
+++ Project_Portofolio/main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import os
+print(os.listdir()) 
 
 # Clear Streamlit cache (you can clear cache programmatically here if needed)
 st.cache_data.clear()
